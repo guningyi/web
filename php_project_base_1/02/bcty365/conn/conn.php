ce6098c8a978ef36bc33dc06c5cd0fbb1ca713ed
guningyi <ningyigu@gmail.com>
2013-06-13 21:25:52 +0800

    first commit

---
+++ php_project_base_1/02/bcty365/conn/conn.php
@@ -0,0 +1,5 @@
+<?php
+ $conn=mysql_connect("localhost","root","123456");
+ mysql_select_db("db_bcty365",$conn);
+ mysql_query("set names gb2312");
+?>
---
+++ php_project_base_1/08/tmlog/Conn/conn.php
@@ -0,0 +1,5 @@
+<?php
+$link=mysql_connect("localhost","root","123456");
+mysql_select_db("db_tmlog",$link);
+mysql_query("set names gb2312");
+?>
